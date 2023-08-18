ebc23460c7b9162b680928fd876abc034c5b5baa
Narmin <mnermin064@gmail.com>
2023-08-18 12:52:59 +0400

    LocalStorage with Root Colors

--- lesson4/assets/js/index.js
+++ lesson4/assets/js/index.js
@@ -1,13 +1,21 @@
 const toggleBtn = document.querySelector("button");
+let img = document.querySelector('img')
+let imgSrc= localStorage.getItem('Image')
+img.setAttribute('src', imgSrc)
+
 
 const enableDarkMode = () => {
   document.body.classList.add("dark");
+  document.querySelector('img').setAttribute('src', "./assets/logo-black.png")
   localStorage.setItem("DarkMode", "active");
+  localStorage.setItem('Image', './assets/logo-black.png')
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark");
+  document.querySelector('img').setAttribute('src', "./assets/logo-white.png")
   localStorage.setItem("DarkMode", null);
+  localStorage.setItem('Image', './assets/logo-white.png')
 };
 
 let myDarkMode = localStorage.getItem("DarkMode");
